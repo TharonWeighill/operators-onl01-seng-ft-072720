833a0196b34aa6b82478c20ff5c577bea5356408
Tharon Weighill <tharonweighill@gmail.com>
2020-07-29 04:06:29 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
+# declare a method called unsafe?
+# the method unsafe? takes in a argument of a speed
+
+# return true if >60
+# return true if <40
+# return false 40 >= || <= 60
+
+require 'pry'
+
 def unsafe?(speed)
-
+ 
+ 
+  binding.pry
 end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
 
